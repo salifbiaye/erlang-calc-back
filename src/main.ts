ae7b67bab2d5d6879a7b4160cffd509fd058feae
Salif Biaye <salifbiaye@esp.sn>
2025-07-06 18:06:02 +0000

    update the main.ts to configure cors access

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-import { AppFactory } from './AppFactory.js';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AppFactory } from './AppFactory';
 
 async function bootstrap() {
-  const { appPromise } = AppFactory.create();
-  const app = await appPromise;
+  let app;
+  if (process.env.NODE_ENV === 'production') {
+    const { appPromise } = AppFactory.create();
+    app = await appPromise;
+  }else{
+    app = await NestFactory.create(AppModule);
+  }
+  
+    // Configuration CORS
+    app.enableCors({
+      origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
+  
 }
 bootstrap();
